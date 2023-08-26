113362bb0d8bfff01a9455e12ed9169f8569b9cf
Ak Moorthi <codxmoothi@gmail.com>
2023-08-26 00:14:32 -0700

    Json Server

---
+++ api/server.js
@@ -0,0 +1,10 @@
+import { create, router as _router, defaults } from 'json-server';
+const server = create();
+const router = _router('db.json');
+const middlewares = defaults();
+const port = process.env.PORT || 3000;
+
+server.use(middlewares);
+server.use(router);
+
+server.listen(port);
