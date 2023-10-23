ae0f004b72af5c173c5f03eff49b54059aa40e96
Ak <moorthimt15@gmail.com>
2023-10-23 19:33:20 +0530

    path change

--- server.js
+++ server.js
@@ -1,7 +1,8 @@
 const jsonServer = require('json-server');
 const server = jsonServer.create();
 const middlewares = jsonServer.defaults();
-const fs = require('fs')
+const fs = require('fs');
+const path = require('path');
 const db = JSON.parse(fs.readFileSync(path.join(__dirname, 'db.json')))
 const router = jsonServer.router(db)
 // const router = jsonServer.router('db.json');
