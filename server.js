cb9198cac2c67d920bfdd9a261bbac2de87ce565
Ak <moorthimt15@gmail.com>
2023-10-23 19:16:24 +0530

    change permision

--- server.js
+++ server.js
@@ -1,7 +1,10 @@
 const jsonServer = require('json-server');
 const server = jsonServer.create();
-const router = jsonServer.router('db.json');
 const middlewares = jsonServer.defaults();
+const fs = require('fs')
+const db = JSON.parse(fs.readFileSync(path.join(__dirname, 'db.json')))
+const router = jsonServer.router(db)
+// const router = jsonServer.router('db.json');
 
 server.use(middlewares);
 // Add this before server.use(router)
